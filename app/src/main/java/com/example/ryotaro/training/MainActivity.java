5e2d121b9f3ba75c5e6f6b9e09287080d8313033
amtk75 <amemiya.takuya.3898@gmail.com>
2017-07-22 17:23:57 +0900

    コメント１

--- app/src/main/java/com/example/ryotaro/training/MainActivity.java
+++ app/src/main/java/com/example/ryotaro/training/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //,kyh,mygmjgj,g,j
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
