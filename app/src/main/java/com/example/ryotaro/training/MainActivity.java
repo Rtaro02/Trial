ff3ce4251335df06482ff830e0b4c199ff1e59d4
noboru <goldrising72@gmail.com>
2017-07-22 20:16:39 +0900

    bottun step1

--- app/src/main/java/com/example/ryotaro/training/MainActivity.java
+++ app/src/main/java/com/example/ryotaro/training/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,7 +16,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn = (Button)findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // クリック時の処理
+            }
+        });
     }
 
-    
+
+
+
 }
